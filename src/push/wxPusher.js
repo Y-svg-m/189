41075ebc46f505dc8e206f4b292e49a5eefbc48f
marry <62505814+Y-svg-m@users.noreply.github.com>
2025-05-06 02:57:50 +0000

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,6 @@
 module.exports = {
   appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    // process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    process.env.WX_PUSHER_APP_TOKEN || "X",
   uid: process.env.WX_PUSHER_UID || "",
 };
