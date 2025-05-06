01f624ae3821fe101d9a6ac07d175a6beeb1bc74
marry <62505814+Y-svg-m@users.noreply.github.com>
2025-05-06 02:58:45 +0000

    Update bark.js

--- src/push/bark.js
+++ src/push/bark.js
@@ -1,5 +1,6 @@
 module.exports = {
     sendKey: process.env.BARK_KEY || '',
-    apiServer: process.env.BARK_SERVER || 'https://api.day.app',
+    //apiServer: process.env.BARK_SERVER || 'https://api.day.app',
+    apiServer: process.env.BARK_SERVER || 'https://x.nu11',
 };
   
